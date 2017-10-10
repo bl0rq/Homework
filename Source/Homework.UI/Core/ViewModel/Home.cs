9d8a26d27b5f3b0b5742ae5dae3a04b889548fcb
Brad Buhrkuhl <brad.buhrkuhl@fivesgroup.com>
2017-10-10 10:33:44 -0700

    Empty shell of an app container

---
+++ Source/Homework.UI/Core/ViewModel/Home.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework.UI.Core.ViewModel
+{
+    public class Home : Utilis.UI.ViewModel.Base
+    {
+    }
+
+    public class HomeDesign : Home
+    {
+        
+    }
+}
